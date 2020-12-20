d5e4b340c38e6b8814fcb784d84822457f3befda
Manav Desai <manavdesai30399@gmail.com>
2020-12-20 17:41:01 +0530

    build the app

---
+++ build/precache-manifest.04e28d5918948e6109849e767e1b5b98.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951266d1eca3a224296baa485d9d1b7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb14a104a67e09a68669",
+    "url": "/static/css/main.fcd74990.chunk.css"
+  },
+  {
+    "revision": "3f1866b44893237dd927",
+    "url": "/static/js/2.d509f8a0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.d509f8a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb14a104a67e09a68669",
+    "url": "/static/js/main.4c5c0b9e.chunk.js"
+  },
+  {
+    "revision": "59f6a3c4c974779a0f65",
+    "url": "/static/js/runtime-main.730a6f76.js"
+  }
+]);
